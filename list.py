1d17b9bb251533ab3eb09149dcae29608043cd59
Soham Umap <24umaps@rbunagpur.in>
2026-03-13 19:50:10 +0530

    list.py

--- list.py
+++ list.py
@@ -11,3 +11,28 @@ print(mylist[1:])
 print(mylist[1:8:2])
 print(mylist[:])
 print(mylist[::-1])
+mylist = ["poorvi","Ashish","sandip","pankaj","shweta"]
+mylist.append('harsh')
+mylist.append("laxman")
+print(mylist)
+
+
+mylist = ["poorvi","Ashish","sandip","pankaj","shweta"]
+mylist.insert(1,"sanket")
+print(mylist)
+
+
+mylist = ["poorvi","Ashish","sandip","pankaj","shweta"]
+mylist.remove("sandip")
+print(mylist)
+
+
+mylist = ["poorvi","Ashish","sandip","pankaj","shweta"]
+newlist = mylist.copy() #cloning
+print(mylist)
+print(newlist)
+
+
+mylist = [['prashant','jha'],['85.56'],[440022,'yyy']]
+print("Example of multidimensional list:")
+print(mylist)
