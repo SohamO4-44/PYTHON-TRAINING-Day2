7145e4f717e82e8cecd0b8990237b18d2bc973e7
Soham Umap <24umaps@rbunagpur.in>
2026-03-13 19:53:13 +0530

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,16 @@
+operator- membership operator [1]in [2]not in
+name = 'pashant'
+print('2'in name)
+print('2' not in name)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
+
+for i in range(5,0,-1):
+    print(i)
+
+for i in range(1,11):
+    print(i*2,"|",i*3, "|",i*4)
